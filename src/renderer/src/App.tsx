4b593925174c58c66a3933bc63a9d0a4ac475f49
sandeep-menon <thesandeepmenon@gmail.com>
2026-06-03 15:40:40 +0100

    adding shadcnui

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,14 @@
+import { toast } from "sonner";
+import { Button } from "./components/ui/button";
+
 export default function App() {
+  const handleClickMe = () => {
+    toast.success("You clicked me!");
+  }
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex flex-col gap-2 justify-center items-center">
       <div>Hello, world!</div>
+      <Button variant={"outline"} onClick={handleClickMe}>Click me!</Button>
     </div>
   )
 }
